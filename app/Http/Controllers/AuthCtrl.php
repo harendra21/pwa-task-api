53a1a6c727d9653186823cc295754fbdf6eb638e
queppelin-dev <bidding@queppelintech.com>
2020-06-21 17:11:46 +0530

    token

--- app/Http/Controllers/AuthCtrl.php
+++ app/Http/Controllers/AuthCtrl.php
@@ -42,8 +42,8 @@ class AuthCtrl extends Controller
         $user = $request->user();
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
-        if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addWeeks(1);
+        //if ($request->remember_me)
+        $token->expires_at = Carbon::now()->addWeeks(1);
         $token->save();
         $data = [
             'access_token' => $tokenResult->accessToken,
